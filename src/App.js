73949eb2570e2a05b07dfbf268a60f3012bea495
Lee Peters <lpeters0023@gmail.com>
2020-08-10 22:14:32 -0400

    finds all pairs and attempts to find unique pairs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from "react";
+import { Text, Flex, Box, Button } from "rebass";
+import { flatten } from 'lodash'
+
+const arr = [1, 9, 1, 5, 4, 4, 5, 5, 5, 6, 7, 6, 1]
+
+
 
 function App() {
+  const findAllPairs = (e) => {
+      let pairs = []
+      const sortedArr = arr.sort((a, b) => a - b)
+      sortedArr.map(first => {
+        sortedArr.map(second => {
+          if (first + second === 10) {
+            pairs.push([first, second])
+          }
+        })
+      })
+      console.log(pairs)
+    }
+
+    const findUniquePairs = (e) => {
+        let pairs = []
+        const sortedArr = arr.sort((a, b) => a - b)
+        sortedArr.map(first => {
+          sortedArr.map(second => {
+            if (first + second === 10) {
+              pairs.push([first, second])
+            }
+          })
+        })
+      }
+
+      
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Flex>
+        <Button
+          cursor="pointer"
+          fontWeight="600"
+          margin="25px"
+          backgroundColor="pink"
+          height="50px"
+          onClick={e => findAllPairs(e)}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          All Pairs
+      </Button>
+      <Button
+        cursor="pointer"
+          fontWeight="600"
+          margin="25px"
+          backgroundColor="pink"
+          height="50px"
+          onClick={e => findUniquePairs(e)}
+        >
+          Unique Pairs
+      </Button>
+        
+    </Flex >
   );
 }
 
